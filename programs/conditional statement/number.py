c759c6fd91af2c9bb097d64e7cf1b195d089de92
sandeepk Jangir <sjangir1208@gmail.com>
2026-02-06 10:00:31 +0530

    Create number.py

---
+++ programs/conditional statement/number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if number > 0:
+    print("The number is Positive")
+    if number % 2 == 0:
+        print("It is Even")
+    else:
+        print("It is Odd")

153add5e797cedf0a21cf22b2c672dd2e2a9b6c5
sandeepk Jangir <sjangir1208@gmail.com>
2026-03-11 15:38:44 +0530

    Add comment for data creation and export functionality
    Added a comment indicating the purpose of the file.

--- Python Libraries/Pandas/student.py
+++ Python Libraries/Pandas/student.py
@@ -1,3 +1,5 @@
+# create a data and export in csv and excel file
+
 student = {
     "student_id" : [1,2,3,4,5,6,7,8,9,10],
     "student_name" : ['ram','shyam','rishi','vikas','abhishek','sandeep','kushagra','mehul','deepak','govind'],
